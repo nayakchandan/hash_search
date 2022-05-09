2a816b06e0a54b4aa4047dcb1b0393e21fba2c8d
Chandan Nayak <54330910+nayakchandan@users.noreply.github.com>
2022-05-09 00:28:21 -0700

    Update htable.py

--- htable.py
+++ htable.py
@@ -24,24 +24,12 @@ def hashcode(o):
             h=h*31+ord(o[i])
         return h
 
-def bucket_indexof(table, key):
-    """
-    You don't have to implement this, but I found it to be a handy function.
-    Return the index of the element within a specific bucket; the bucket is:
-    table[hashcode(key) % len(table)]. You have to linearly
-    search the bucket to find the tuple containing key.
-    """
-    pass
+
 
 def htable_put(table, key, value):
     """
     Perform the equivalent of table[key] = value
-    Find the appropriate bucket indicated by key and then append (key,value)
-    to that bucket if the (key,value) pair doesn't exist yet in that bucket.
-    If the bucket for key already has a (key,value) pair with that key,
-    then replace the tuple with the new (key,value).
-    Make sure that you are only adding (key,value) associations to the buckets.
-    The type(value) can be anything. Could be a set, list, number, string, anything!
+
     """
     nbuckets = len(table) # first we find the lenght of the hashtable, to use the modulo
     bucket = hashcode(key)%nbuckets # find the bucket using the hash function on key and then compressing
@@ -62,30 +50,6 @@ def htable_put(table, key, value):
             search_region.append((key,{value}))
         else:
             search_region.append((key,value))
-
-"""
-def htable_put(table, key, value):
-
-    nbuckets = len(table) # first we find the lenght of the hashtable, to use the modulo
-    bucket = hashcode(key)%nbuckets # find the bucket using the hash function on key and then compressing
-
-    # This gives the list containing all keys with the same encodings.
-    search_region = table[bucket]
-    # Linear search begins
-    counter = []
-    for element in search_region:
-        k,v = element
-        if k == key:
-            counter.append(k)
-            v.add(value)
-            break # exit as no need to search any further
-    if key not in counter: # no match found then create a new key,value pair
-        #search_region.append(((key,{value})))
-        if type(value)== int:
-            search_region.append((key,{value}))
-        else:
-            search_region.append((key,value))
-"""
 
 def htable_get(table, key):
     """
@@ -109,16 +73,7 @@ def htable_get(table, key):
 
 
 def htable_buckets_str(table):
-    """
-    Return a string representing the various buckets of this table.
-    The output looks like:
-        0000->
-        0001->
-        0002->
-        0003->parrt:99
-        0004->
-    where parrt:99 indicates an association of (parrt,99) in bucket 3.
-    """
+
     content = ''
     for i in range(len(table)):
         #bucket = table[i]
@@ -139,12 +94,7 @@ def htable_buckets_str(table):
 
 
 def htable_str(table):
-    """
-    Return what str(table) would return for a regular Python dict
-    such as {parrt:99}. The order should be in bucket order and then
-    insertion order within each bucket. The insertion order is
-    guaranteed when you append to the buckets in htable_put().
-    """
+
     result = ''
     for i in range(len(table)):
         bucket = table[i]
@@ -163,23 +113,6 @@ def htable_str(table):
     else:
         return "{"+result[:len(result)-2]+"}"
 
-"""
-    result = ''
-    for i in range(len(table)):
-        bucket = table[i]
-        for pairs in bucket:
-            if len(pairs) == 0:
-                result = result +''
-            else:
-                key = pairs[0]
-                value = pairs[1]
-                result = result+str(key)+':'+str(list(value)[0])+", "
-
-
-    if result == '':
-        return "{}"
-    else:
-        return "{"+result[:len(result)-2]+"}"
 """
     
     
