63adf9bf33fb6ed3007facce6ec80e9afb01d282
Chandan Nayak <54330910+nayakchandan@users.noreply.github.com>
2022-05-09 00:29:02 -0700

    Update index_search.py

--- index_search.py
+++ index_search.py
@@ -6,10 +6,7 @@ from words import get_text, words
 def create_index(files):
     """
     Given a list of fully-qualified filenames, build an index from word
-    to set of document IDs. A document ID is just the index into the
-    files parameter (indexed from 0) to get the file name. Make sure that
-    you are mapping a word to a set of doc IDs, not a list.
-    For each word w in file i, add i to the set of document IDs containing w
+    to set of document IDs.
     Return a dict object mapping a word to a set of doc IDs.
     """
     index = defaultdict(list) #dict object containing the list of data
@@ -30,9 +27,7 @@ def index_search(files, index, terms):
     """
     Given an index and a list of fully-qualified filenames, return a list of
     filenames whose file contents has all words in terms parameter as normalized
-    by your words() function.  Parameter terms is a list of strings.
-    You can only use the index to find matching files; you cannot open the files
-    and look inside.
+    by your words() function.  
     """
     # get a list of all files that contain each search term and append a list
     common_words = []
